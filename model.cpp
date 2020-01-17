7a0e946f5eceac66554bd708654557744fe7aa9e
Curtis Mason <cumason@bu.edu>
2020-01-17 04:46:59 -0500

    Added env variables

--- model.cpp
+++ model.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include "include/twitcurl.h"
+
 int main() {
-	std::cout << "Hello World!" << std::endl;
+	twitCurl twit;
+	twit.setTwitterUsername(std::getenv("TWITTER_USERNAME"));
+	twit.setTwitterPassword(std::getenv("TWITTER_PASSWORD"));
+	std::cout << "Set twitter login creds" << std::endl;
 }
