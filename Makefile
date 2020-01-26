94dab2a92981a6fc64842546ed31eac99c4ad4b2
Curtis Mason <cumason@bu.edu>
2020-01-25 19:27:55 -0500

    Staged updated Makefile and model.cpp for commit

--- Makefile
+++ Makefile
@@ -3,7 +3,13 @@ make:
 image:
 	docker build -f Dockerfile .
 container:
-	docker run -i -t $(image) /bin/bash
+	docker run \
+	-e TWITTER_PASSWORD= ${TWITTER_PASSWORD} \
+	-e TWITTER_USERNAME=${TWITTER_USERNAME} \
+	-e TWITTER_API_KEY=${TWITTER_API_KEY} \
+	-e TWITTER_API_SECRET=${TWITTER_API_SECRET} \
+	-v ~/workspace/political-bias-discriminator/model.cpp:/usr/src/political-bias-discriminator/model.cpp \
+	-i -t $(image) /bin/bash
 clean:
 	rm *.o
 	rm *.a
--- model.cpp
+++ model.cpp
@@ -6,46 +6,43 @@ twitCurl oAuthFlow() {
 
 	std::string authUrl;
 	std::string replyMsg;
-    std::string myOAuthAccessTokenKey("");
-    std::string myOAuthAccessTokenSecret("");
+	std::string myOAuthAccessTokenKey("");
+	std::string myOAuthAccessTokenSecret("");
 
 	char tmpBuf[1024];
-    memset( tmpBuf, 0, 1024 );
+	memset( tmpBuf, 0, 1024 );
 
 	twit.setTwitterUsername(std::getenv("TWITTER_USERNAME"));
 	twit.setTwitterPassword(std::getenv("TWITTER_PASSWORD"));
 
 	/* OAuth flow begins */
-    /* Step 0: Set OAuth related params. These are got by registering your app at twitter.com */
-    twit.getOAuth().setConsumerKey(std::getenv("TWITTER_API_KEY"));
-    twit.getOAuth().setConsumerSecret(std::getenv("TWITTER_API_SECRET"));
+	/* Step 0: Set OAuth related params. These are got by registering your app at twitter.com */
+	twit.getOAuth().setConsumerKey(std::getenv("TWITTER_API_KEY"));
+	twit.getOAuth().setConsumerSecret(std::getenv("TWITTER_API_SECRET"));
+	printf("Cool");
+	/* Step 2: Get request token key and secret */
+	twit.oAuthRequestToken(authUrl);
 
-    /* Step 2: Get request token key and secret */
-    twit.oAuthRequestToken(authUrl);
+	/* Step 3: Get PIN  */
+	twit.oAuthHandlePIN(authUrl);
 
-    /* Step 3: Get PIN  */
-    twit.oAuthHandlePIN(authUrl);
+	/* Step 4: Exchange request token with access token */
+	twit.oAuthAccessToken();
 
-    /* Step 4: Exchange request token with access token */
-    twit.oAuthAccessToken();
-
-    /* Step 5: Now, save this access token key and secret for future use without PIN */
-    twit.getOAuth().getOAuthTokenKey(myOAuthAccessTokenKey);
-    twit.getOAuth().getOAuthTokenSecret(myOAuthAccessTokenSecret);
-    
-    /* Check reply */
-
-    /* Account credentials verification */
-    if(twit.accountVerifyCredGet()) { 
-        twit.getLastWebResponse(replyMsg);
-        printf("\nmodel::twitCurl::accountVerifyCredGet web response:\n%s\n", replyMsg.c_str());
-    }
-    else {
-        twit.getLastCurlError(replyMsg);
-        printf("\nmodel::twitCurl::accountVerifyCredGet error:\n%s\n", replyMsg.c_str());
-    }
-
-
+	/* Step 5: Now, save this access token key and secret for future use without PIN */
+	twit.getOAuth().getOAuthTokenKey(myOAuthAccessTokenKey);
+	twit.getOAuth().getOAuthTokenSecret(myOAuthAccessTokenSecret);
+	
+	/* Check reply */
+	/* Account credentials verification */
+	if(twit.accountVerifyCredGet()) { 
+		twit.getLastWebResponse(replyMsg);
+		printf("\nmodel::twitCurl::accountVerifyCredGet web response:\n%s\n", replyMsg.c_str());
+	}
+	else {
+		twit.getLastCurlError(replyMsg);
+		printf("\nmodel::twitCurl::accountVerifyCredGet error:\n%s\n", replyMsg.c_str());
+	}
 }
 
 int main() {
