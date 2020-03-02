37ea6fa060c8f88a676887880c7d170f72211a3c
Curtis Mason <cumason@bu.edu>
2020-03-02 15:44:49 -0500

    debugging model.cpp

--- model.cpp
+++ model.cpp
@@ -2,10 +2,9 @@
 #include <tensorflow/c/c_api.h>
 #include <stdio.h>
 #include <iostream>
-twitCurl oAuthFlow() {
+twitCurl *oAuthFlow() {
 	twitCurl twit;
 
-	std::string authUrl;
 	std::string replyMsg;
 	std::string myOAuthAccessTokenKey("");
 	std::string myOAuthAccessTokenSecret("");
@@ -18,32 +17,36 @@ twitCurl oAuthFlow() {
 
 	/* OAuth flow begins */
 	/* Step 0: Set OAuth related params. These are got by registering your app at twitter.com */
+	printf("Beginning auth with %s, %s\n", std::getenv("TWITTER_API_KEY"), std::getenv("TWITTER_API_SECRET"));
+	printf("Beginning auth with %s, %s\n", std::getenv("TWITTER_API_KEY"), std::getenv("TWITTER_API_SECRET"));
 	twit.getOAuth().setConsumerKey(std::getenv("TWITTER_API_KEY"));
 	twit.getOAuth().setConsumerSecret(std::getenv("TWITTER_API_SECRET"));
-	printf("Cool");
+
 	/* Step 2: Get request token key and secret */
-	twit.oAuthRequestToken(authUrl);
+	std::string authUrl;
+    twit.oAuthRequestToken( authUrl );
+	printf("Cool\n");
 
 	/* Step 3: Get PIN  */
-	twit.oAuthHandlePIN(authUrl);
+	// twit.oAuthHandlePIN(authUrl);
 
 	/* Step 4: Exchange request token with access token */
-	twit.oAuthAccessToken();
+	// twit.oAuthAccessToken();
 
-	/* Step 5: Now, save this access token key and secret for future use without PIN */
-	twit.getOAuth().getOAuthTokenKey(myOAuthAccessTokenKey);
-	twit.getOAuth().getOAuthTokenSecret(myOAuthAccessTokenSecret);
-	
-	/* Check reply */
-	/* Account credentials verification */
-	if(twit.accountVerifyCredGet()) { 
-		twit.getLastWebResponse(replyMsg);
-		printf("\nmodel::twitCurl::accountVerifyCredGet web response:\n%s\n", replyMsg.c_str());
-	}
-	else {
-		twit.getLastCurlError(replyMsg);
-		printf("\nmodel::twitCurl::accountVerifyCredGet error:\n%s\n", replyMsg.c_str());
-	}
+	// /* Step 5: Now, save this access token key and secret for future use without PIN */
+	// twit.getOAuth().getOAuthTokenKey(myOAuthAccessTokenKey);
+	// twit.getOAuth().getOAuthTokenSecret(myOAuthAccessTokenSecret);
+	// /* Check reply */
+	// /* Account credentials verification */
+	// if(twit.accountVerifyCredGet()) { 
+	// 	twit.getLastWebResponse(replyMsg);
+	// 	printf("\nmodel::twitCurl::accountVerifyCredGet web response:\n%s\n", replyMsg.c_str());
+	// }
+	// else {
+	// 	twit.getLastCurlError(replyMsg);
+	// 	printf("\nmodel::twitCurl::accountVerifyCredGet error:\n%s\n", replyMsg.c_str());
+	// }
+	return NULL;
 }
 
 int main() {
@@ -53,6 +56,6 @@ int main() {
 		printf("[TWITTER_USERNAME, TWITTER_PASSWORD, TWITTER_API_KEY, TWITTER_API_SECRET]\n");
 		exit(1);
 	}
-	twitCurl twit = oAuthFlow();
+	twitCurl* twit = oAuthFlow();
 	printf("Hello from TensorFlow C library version %s\n", TF_Version());
 }
